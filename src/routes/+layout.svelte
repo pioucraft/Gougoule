968e90119e127ad103fd4a38ccd444e340b41358
pfannkuchen <hello@gougoule.ch>
2024-09-14 21:07:44 +0200

    things

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script>
+    import { supabase } from "$lib/supabase";
     import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
     import "../app.css";
     import { autoModeWatcher } from "@skeletonlabs/skeleton";
-    onMount(() => {
+
+    onMount(async () => {
         autoModeWatcher();
+        let { error } = await supabase.auth.getUser();
+        if (error) goto("/login");
     });
 </script>
 
